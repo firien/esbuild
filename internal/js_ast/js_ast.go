a7bb29f6a3f744ffb0620abb6579ec483c10c36c
Patrick Griffin <58729+firien@users.noreply.github.com>
2022-08-14 15:07:38 +0000

    relative url

--- internal/js_ast/js_ast.go
+++ internal/js_ast/js_ast.go
@@ -466,6 +466,7 @@ func (*EIf) isExpr()                   {}
 func (*ERequireString) isExpr()        {}
 func (*ERequireResolveString) isExpr() {}
 func (*EImportString) isExpr()         {}
+func (*ERelativeURL) isExpr()         {}
 func (*EImportCall) isExpr()           {}
 
 type EArray struct {
@@ -884,6 +885,10 @@ type ERequireString struct {
 type ERequireResolveString struct {
 	ImportRecordIndex uint32
 	CloseParenLoc     logger.Loc
+}
+
+type ERelativeURL struct {
+	ImportRecordIndex uint32
 }
 
 type EImportString struct {
